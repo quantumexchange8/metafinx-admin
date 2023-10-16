49f3f3bc01664f9c3ad5f034456d239b0710979d
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-10-16 17:12:28 +0800

    fix: button component

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -61,39 +61,39 @@ const baseClasses = [
 ]
 
 const variantClasses = (variant) => ({
-    'bg-pink-500 text-white hover:bg-pink-600': variant == 'primary',
+    'bg-pink-500 text-white hover:bg-pink-600': variant === 'primary',
     'bg-white border border-gray-300 text-gray-600 hover:bg-gray-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:bg-[#1B2530]':
-        variant == 'secondary',
-    'bg-success-600 hover:bg-success-700 text-white': variant == 'success',
-    'bg-error-600 text-white hover:bg-error-700': variant == 'danger',
-    'bg-warning-400 text-white hover:bg-warning-500': variant == 'warning',
-    'bg-gray-600 border border-gray-500 text-white': variant == 'info',
+        variant === 'secondary',
+    'bg-success-600 hover:bg-success-700 text-white': variant === 'success',
+    'bg-error-600 text-white hover:bg-error-700': variant === 'danger',
+    'bg-warning-400 text-white hover:bg-warning-500': variant === 'warning',
+    'bg-gray-600 border border-gray-500 text-white': variant === 'info',
     'bg-gray-400 hover:bg-gray-500 text-white dark:bg-gray-500 dark:hover:bg-gray-600':
-        variant == 'gray',
+        variant === 'gray',
     'text-gray-400 bg-transparent dark:hover:text-white':
-        variant == 'transparent',
-    'bg-gray-400 hover:bg-gray-500 text-white dark:bg-gray-600 dark:hover:bg-gray-500':
-        variant == 'action',
+        variant === 'transparent',
+    'bg-gray-400 hover:bg-gray-500 text-white dark:bg-gray-600 dark:hover:bg-gray-800':
+        variant === 'action',
 })
 
 const classes = computed(() => [
     ...baseClasses,
     iconOnly
         ? {
-            'p-1.5': size == 'sm',
-            'p-2': size == 'base',
-            'p-3': size == 'lg',
+            'p-1.5': size === 'sm',
+            'p-2': size === 'base',
+            'p-3': size === 'lg',
         }
         : {
-            'px-2.5 py-1.5 text-sm': size == 'sm',
-            'px-4 py-2 text-base': size == 'base',
-            'px-5 py-2 text-xl': size == 'lg',
+            'px-2.5 py-1.5 text-sm': size === 'sm',
+            'px-4 py-2 text-base': size === 'base',
+            'px-5 py-2 text-xl': size === 'lg',
         },
     variantClasses(variant),
-    // {
-    //     'rounded-md': !squared && !pill,
-    //     'rounded-full': pill,
-    // },
+    {
+        'rounded-md': !squared && !pill,
+        'rounded-full': pill,
+    },
     {
         'pointer-events-none opacity-50': href && disabled.value,
     },
@@ -101,9 +101,9 @@ const classes = computed(() => [
 
 const iconSizeClasses = [
     {
-        'w-5 h-5': size == 'sm',
-        'w-6 h-6': size == 'base',
-        'w-7 h-7': size == 'lg',
+        'w-5 h-5': size === 'sm',
+        'w-6 h-6': size === 'base',
+        'w-7 h-7': size === 'lg',
     },
 ]
 
