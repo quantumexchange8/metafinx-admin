f66b6e855ecf3229e7dc8646e1fe3aa176e4fc30
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2024-02-13 16:10:19 +0800

    fix: member details fix button alignment

--- resources/js/Pages/Member/MemberDetails/Partials/Action.vue
+++ resources/js/Pages/Member/MemberDetails/Partials/Action.vue
@@ -70,7 +70,7 @@ const openInNewTab = (url) => {
                 <span class="sr-only">Unsubscribe</span>
             </Button>
         </Tooltip>
-        <div class="flex flex-col space-y-2">
+        <div class="flex flex-col sm:flex-row gap-3">
             <Button
                 type="button"
                 class="justify-center px-3 py-2 gap-2 grow focus:outline-none"
